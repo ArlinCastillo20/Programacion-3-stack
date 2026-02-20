38b5e6577ba11fa9f72a1daa8d89b46888826e34
Arlin Castillo <acastilloc31@miumg.edu.gt>
2026-02-20 19:36:41 +0100

    Proyecto stack funcionando

--- umg.edu.gt.data-structure.stack/src/main/java/umg/edu/gt/datastructure/stack/App.java
+++ umg.edu.gt.data-structure.stack/src/main/java/umg/edu/gt/datastructure/stack/App.java
@@ -3,13 +3,19 @@ package umg.edu.gt.datastructure.stack;
 public class App {
     public static void main(String[] args) {
 
-        String test1 = "{[()]}";
-        String test2 = "{[(])}";
+    MyStack stack = new MyStack();
 
-        System.out.println("Prueba 1: " + test1);
-        System.out.println("Es válida? " + SymbolValidator.isValid(test1));
+    stack.push('a');
+    stack.push('b');
+    stack.push('c');
 
-        System.out.println("Prueba 2: " + test2);
-        System.out.println("Es válida? " + SymbolValidator.isValid(test2));
-    }
+    System.out.println("Cantidad: " + stack.getCount());
+
+    Node n = stack.getNodeInit();
+    System.out.println("Nodo inicial: " + (n != null ? n.data : "null"));
+
+    System.out.println("Pop: " + stack.pop());
+    System.out.println("Cantidad luego pop: " + stack.getCount());
+}
+
 }
--- umg.edu.gt.data-structure.stack/src/main/java/umg/edu/gt/datastructure/stack/MyStack.java
+++ umg.edu.gt.data-structure.stack/src/main/java/umg/edu/gt/datastructure/stack/MyStack.java
@@ -3,27 +3,40 @@ package umg.edu.gt.datastructure.stack;
 public class MyStack {
 
     private Node top;
+    private int count;
 
     public MyStack() {
         this.top = null;
+        this.count = 0;
     }
 
     public void push(char data) {
         Node newNode = new Node(data);
         newNode.next = top;
         top = newNode;
+        count++;
     }
 
     public char pop() {
         if (isEmpty()) {
             return '\0';
         }
+
         char data = top.data;
         top = top.next;
+        count--;
         return data;
     }
 
     public boolean isEmpty() {
         return top == null;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public Node getNodeInit() {
+        return top;
     }
 }
