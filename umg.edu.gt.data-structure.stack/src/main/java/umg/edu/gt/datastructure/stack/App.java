9ce6eca4a5a20c0f46e7a98b4c66f76a74a15188
Arlin Castillo <acastilloc31@miumg.edu.gt>
2026-02-20 13:06:06 +0100

    Implementacion de Stack funcinando

--- umg.edu.gt.data-structure.stack/src/main/java/umg/edu/gt/datastructure/stack/App.java
+++ umg.edu.gt.data-structure.stack/src/main/java/umg/edu/gt/datastructure/stack/App.java
@@ -1,13 +1,54 @@
 package umg.edu.gt.datastructure.stack;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    static class Stack {
+        private int[] data;
+        private int top;
+
+        public Stack(int capacity) {
+            data = new int[capacity];
+            top = -1;
+        }
+
+        public void push(int value) {
+            if (top == data.length - 1) {
+                System.out.println("Stack llena");
+                return;
+            }
+            data[++top] = value;
+            System.out.println("Push: " + value);
+        }
+
+        public int pop() {
+            if (top == -1) {
+                System.out.println("Stack vacía");
+                return -1;
+            }
+            return data[top--];
+        }
+
+        public void print() {
+            System.out.print("Contenido: ");
+            for (int i = 0; i <= top; i++) {
+                System.out.print(data[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Demostración de Stack");
+
+        Stack s = new Stack(5);
+
+        s.push(10);
+        s.push(20);
+        s.push(30);
+        s.print();
+
+        System.out.println("Pop: " + s.pop());
+        s.print();
     }
 }
