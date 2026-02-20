74f88fcb433446069eb5e8adb233655f637557c0
Arlin Castillo <acastilloc31@miumg.edu.gt>
2026-02-20 12:16:24 +0100

    Proyecto Maven Funcionando Build Success

---
+++ umg.edu.gt.data-structure.stack/src/main/java/umg/edu/gt/datastructure/stack/App.java
@@ -0,0 +1,13 @@
+package umg.edu.gt.datastructure.stack;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
